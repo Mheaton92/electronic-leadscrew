ea7dd6d3fc302f884bcae9be507ae6155d7c9ea8
DGS <47985749+Mheaton92@users.noreply.github.com>
2023-05-12 09:19:42 -0400

    Update Configuration.h
    Updated for PM-1030

--- els-f280049c/Configuration.h
+++ els-f280049c/Configuration.h
@@ -40,7 +40,7 @@
 //================================================================================
 
 // For Imperial leadscrews: pitch in Threads Per Inch (TPI)
-#define LEADSCREW_TPI 12
+#define LEADSCREW_TPI 10
 
 // For metric leadscrews: pitch in hundredths of a millimeter (HMM)
 // Example: 200hmm = 2mm
@@ -64,12 +64,12 @@
 //================================================================================
 
 // Steps and microsteps
-#define STEPPER_MICROSTEPS 8
-#define STEPPER_RESOLUTION 200
+#define STEPPER_MICROSTEPS 6
+#define STEPPER_RESOLUTION 500
 
 // Separate step and microstep settings for feed rates.  Redefine these if your
 // lathe has a separate feed drive train with a different ratio.
-#define STEPPER_MICROSTEPS_FEED STEPPER_MICROSTEPS
+#define STEPPER_MICROSTEPS_FEED 10
 #define STEPPER_RESOLUTION_FEED STEPPER_RESOLUTION
 
 // Step, direction and enable pins are normally active-high
